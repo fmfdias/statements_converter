d964ce338df42e677fba90500f37bd212c74c42b
Filipe Dias <f.dias@runtime-revolution.com>
2016-08-21 18:16:18 +0100

    Add and setup the libraries needed for the bancobpi converter.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule StatementsConverter.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger,:iconv,:timex]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,10 @@ defmodule StatementsConverter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:floki, "~> 0.10.0"},
+      {:iconv, "~> 1.0"},
+      {:timex, "~> 3.0"}
+    ]
   end
 end
