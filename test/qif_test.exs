d4f32c8be7b8a9d2de72674612b337d5d4fb77f6
Filipe Dias <f.dias@runtime-revolution.com>
2016-08-22 00:50:23 +0100

    Ensure it closes StringIO.

--- test/qif_test.exs
+++ test/qif_test.exs
@@ -9,147 +9,183 @@ defmodule QIFTest do
 
   test "writes Bank as default type" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: nil,
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{type: nil, transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: nil,
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{type: nil, transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "uses type option as type" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: nil,
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{type: "CCard", transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:CCard
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: nil,
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{type: "CCard", transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:CCard
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "formats dates as \"{0D}-{0M}-{YYYY}\" by default" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: ~D[2016-08-06],
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    D06-08-2016
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: ~D[2016-08-06],
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      D06-08-2016
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "formats dates with passed date format" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: ~D[2016-08-06],
-      memo: nil,
-      payee: nil}
-    ]
-    write(
-      %Statement{transactions: transactions}, 
-      io, 
-      [date_format: "{0M}-{0D}-{YYYY}"]
-    )
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    D08-06-2016
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: ~D[2016-08-06],
+        memo: nil,
+        payee: nil}
+      ]
+      write(
+        %Statement{transactions: transactions}, 
+        io, 
+        [date_format: "{0M}-{0D}-{YYYY}"]
+      )
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      D08-06-2016
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "amount is correctly written" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: -3.33, date: nil,
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    T-3.33
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: -3.33, date: nil,
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      T-3.33
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "amounts are rounded to 2 decimal places" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: -3.333, date: nil,
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    T-3.33
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: -3.333, date: nil,
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      T-3.33
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "amounts can be integers" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: 2, date: nil,
-      memo: nil,
-      payee: nil}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    T2.00
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: 2, date: nil,
+        memo: nil,
+        payee: nil}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      T2.00
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "memo is written correctly" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: nil,
-      memo: "Hello memo",
-      payee: nil}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    MHello memo
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: nil,
+        memo: "Hello memo",
+        payee: nil}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      MHello memo
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 
   test "payee is written correctly" do
     {:ok, io} = StringIO.open("")
-    transactions = [%Transaction{amount: nil, date: nil,
-      memo: nil,
-      payee: "Hello payee"}
-    ]
-    write(%Statement{transactions: transactions}, io)
-    result = StringIO.flush(io)
-    StringIO.close(io)
-    assert result == """
-    !Type:Bank
-    PHello payee
-    ^
-    """
+    try do
+      transactions = [%Transaction{amount: nil, date: nil,
+        memo: nil,
+        payee: "Hello payee"}
+      ]
+      write(%Statement{transactions: transactions}, io)
+      result = StringIO.flush(io)
+
+      assert result == """
+      !Type:Bank
+      PHello payee
+      ^
+      """
+    after
+      StringIO.close(io)
+    end
   end
 end
